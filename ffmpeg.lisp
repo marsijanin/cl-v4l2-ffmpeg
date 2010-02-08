08c7171c5df62e6abfcbd091c4422ee29124ffaa
Nikolay V. Razbegaev <marsijanin@gmail.com>
2010-02-08 21:44:39 +0300

    do-frames and with-v4l2-do-frames macros

--- ffmpeg.lisp
+++ ffmpeg.lisp
@@ -30,15 +30,30 @@
        (when (v4l2-openp ,v4l2-var)
 	 (close-v4l2 ,v4l2-var)))))
 
+(defmacro do-frames ((frame-var v4l2 &key end-test-form return-form) &body body)
+  (let ((fd (gensym "fd")))
+    `(do* ((,fd (v4l2-fd ,v4l2))
+	   (,frame-var (w/o-errors (v4l2:get-frame ,fd))
+		       (w/o-errors (v4l2:get-frame ,fd))))
+	  (,end-test-form ,return-form)
+       (when ,frame-var
+	 ,@body
+	 (v4l2:put-frame ,fd ,frame-var)))))
+
+(defmacro with-v4l2-do-frames ((v4l2-var frame-var path &key
+			       (w 640) (h 480) (pixformat v4l2:pix-fmt-rgb24) (n-buffs 4)
+			       end-test-form return-form)
+			       &body body)
+  `(with-v4l2 (,v4l2-var ,path :w ,w :h ,h :pixformat ,pixformat :n-buffs ,n-buffs)
+     (do-frames (,frame-var ,v4l2-var ,end-test-form ,return-form)
+       ,@body)))
+
 (defun write-frames-to-ffmpeg-fifo-w/o-threads (v4l2 fifo-fd)
-  (loop
-     with capture-fd = (v4l2-fd v4l2)
-     with sz = (slot-value (v4l2:format-pix (v4l2:get-image-format capture-fd)) 'v4l2:sizeimage)
-     with capture-buffers = (v4l2-buffers v4l2)
-     for frame    = (w/o-errors (v4l2:get-frame capture-fd))
-     as  buff-ptr = (when frame (second (nth frame capture-buffers)))
-     when buff-ptr do (progn (isys:%sys-write fifo-fd buff-ptr sz)
-			     (v4l2:put-frame capture-fd frame))))
+  (let ((buffs (v4l2-buffers v4l2))
+	(size (slot-value (v4l2:format-pix (v4l2:get-image-format (v4l2-fd v4l2)))
+			  'v4l2:sizeimage)))
+    (do-frames (frame v4l2)
+      (isys:%sys-write fifo-fd (second (nth frame buffs)) size))))
 
 (defun split-video-stream-into-fifos-w/o-threads (v4l2 fifos-and-cmds)
   (loop
